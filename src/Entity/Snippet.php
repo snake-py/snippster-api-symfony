82ba581bf361833e0410a189e9ab32a22a5fc6e3
snake-py <fabio@snake-py.com>
2024-09-28 10:52:45 +0200

    added user

--- src/Entity/Snippet.php
+++ src/Entity/Snippet.php
@@ -5,14 +5,17 @@ namespace App\Entity;
 use App\Repository\SnippetRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity(repositoryClass: SnippetRepository::class)]
 class Snippet
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
+    private ?Uuid $id = null;
 
     #[ORM\Column(length: 255)]
     private ?string $title = null;
